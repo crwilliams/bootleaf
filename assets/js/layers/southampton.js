f6da55687eb68d32ae90e2074c1a83d9a2c29f88
Colin Williams <github@crwilliams.co.uk>
2014-06-22 22:31:21 +0100

    Change order of layers.

--- assets/js/layers/southampton.js
+++ assets/js/layers/southampton.js
@@ -10,15 +10,47 @@ function createSouthamptonLayer(layername, name)
   );
 }
 
-createSouthamptonLayer('Transportation', 'Transport');
-createSouthamptonLayer('Restaurants-and-Hotels', 'Catering-Accommodation');
-createSouthamptonLayer('Offices', 'Services');
-createSouthamptonLayer('Culture-and-Entertainment', 'Culture-Entertainment');
-createSouthamptonLayer('Health', 'Health-Beauty');
-createSouthamptonLayer('Tourism', 'Tourism-Religion');
-createSouthamptonLayer('Stores', 'Retail');
-createSouthamptonLayer('Education', 'Education');
-createSouthamptonLayer('Sports', 'Sports');
+layers['sites'] = {
+  name: 'Sites',
+  datafile: 'data/southampton/sites.geojson',
+  type: 'reference',
+  metadata: southamptonMetadata,
+  initialSelected: true,
+  search: [],
+  geojson: L.geoJson(null, {
+    style: function (feature) {
+      return {
+        color: "black",
+        fill: false,
+        opacity: 1,
+        clickable: true,
+        weight: "3px"
+      };
+    },
+    onEachFeature: function (feature, layer) {
+      handleFeature('sites', feature, layer);
+    }  ,
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, {
+        icon: L.icon({
+          iconUrl: 'http://opendatamap.ecs.soton.ac.uk/img/icon/Media/blank.png',
+          iconSize: [24, 28],
+          iconAnchor: [12, 28],
+          popupAnchor: [0, -25]
+        }),
+        title: feature.properties.NAME,
+        riseOnHover: true
+      });
+    }
+  }),
+  tatemplate: {
+    header: "<h4 class='typeahead-header'>Southampton Sites</h4>"
+  },
+  selected: function(map, datum) {
+    map.fitBounds(datum.bounds);
+  },
+  columns: ['Name']
+};
 
 layers['buildings'] = {
   name: 'Buildings',
@@ -62,47 +94,15 @@ layers['buildings'] = {
   columns: ['Name']
 };
 
-layers['sites'] = {
-  name: 'Sites',
-  datafile: 'data/southampton/sites.geojson',
-  type: 'reference',
-  metadata: southamptonMetadata,
-  initialSelected: true,
-  search: [],
-  geojson: L.geoJson(null, {
-    style: function (feature) {
-      return {
-        color: "black",
-        fill: false,
-        opacity: 1,
-        clickable: true,
-        weight: "3px"
-      };
-    },
-    onEachFeature: function (feature, layer) {
-      handleFeature('sites', feature, layer);
-    }  ,
-    pointToLayer: function (feature, latlng) {
-      return L.marker(latlng, {
-        icon: L.icon({
-          iconUrl: 'http://opendatamap.ecs.soton.ac.uk/img/icon/Media/blank.png',
-          iconSize: [24, 28],
-          iconAnchor: [12, 28],
-          popupAnchor: [0, -25]
-        }),
-        title: feature.properties.NAME,
-        riseOnHover: true
-      });
-    }
-  }),
-  tatemplate: {
-    header: "<h4 class='typeahead-header'>Southampton Sites</h4>"
-  },
-  selected: function(map, datum) {
-    map.fitBounds(datum.bounds);
-  },
-  columns: ['Name']
-};
-
 setTitle('University of Southampton Map');
 var defaultViewport = L.latLngBounds(L.latLng(50.932858999577164, -1.4007300639873421), L.latLng(50.938211037309856, -1.3923620003962731));
+
+createSouthamptonLayer('Transportation', 'Transport');
+createSouthamptonLayer('Restaurants-and-Hotels', 'Catering-Accommodation');
+createSouthamptonLayer('Offices', 'Services');
+createSouthamptonLayer('Culture-and-Entertainment', 'Culture-Entertainment');
+createSouthamptonLayer('Health', 'Health-Beauty');
+createSouthamptonLayer('Tourism', 'Tourism-Religion');
+createSouthamptonLayer('Stores', 'Retail');
+createSouthamptonLayer('Education', 'Education');
+createSouthamptonLayer('Sports', 'Sports');
